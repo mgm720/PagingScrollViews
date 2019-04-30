2db6e93cc01da0f837fec214f332017117cd1f2d
Michael Miles <michael.gage.miles@gmail.com>
2019-04-30 15:14:52 -0700

    Completed tutorial.

---
+++ PagingScrollViews/SlideView.swift
@@ -0,0 +1,17 @@
+//
+//  SlideView.swift
+//  PagingScrollViews
+//
+//  Created by Michael Miles on 4/30/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class SlideView: UIView {
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var colorLabel: UILabel!
+    
+
+}
--- PagingScrollViews/ViewController.swift
+++ PagingScrollViews/ViewController.swift
@@ -9,12 +9,107 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var slides : [SlideView] = []
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageController: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageController.numberOfPages = slides.count
+        pageController.currentPage = 0
+        view.bringSubviewToFront(pageController)
     }
 
+    func createSlides() -> [SlideView] {
+        let slide1 : SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as! SlideView
+        slide1.colorView.backgroundColor = UIColor.blue
+        slide1.colorLabel.text = "Blue"
+        
+        let slide2 : SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as! SlideView
+        slide2.colorView.backgroundColor = UIColor.green
+        slide2.colorLabel.text = "Green"
+        
+        let slide3 : SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as! SlideView
+        slide3.colorView.backgroundColor = UIColor.purple
+        slide3.colorLabel.text = "Purple"
+        
+        let slide4 : SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as! SlideView
+        slide4.colorView.backgroundColor = UIColor.red
+        slide4.colorLabel.text = "Red"
+        
+        let slide5 : SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as! SlideView
+        slide5.colorView.backgroundColor = UIColor.orange
+        slide5.colorLabel.text = "Orange"
+        
+        return [slide1, slide2, slide3, slide4, slide5]
+    }
+    
+    func setupSlideScrollView(slides : [SlideView]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
 
 }
 
+extension ViewController: UIScrollViewDelegate {
+    /*
+     * default function called when view is scolled. In order to enable callback
+     * when scrollview is scrolled, the below code needs to be called:
+     * slideScrollView.delegate = self or
+     */
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageController.currentPage = Int(pageIndex)
+        
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+        
+        
+        /*
+         * below code changes the background color of view on paging the scrollview
+         */
+        //        self.scrollView(scrollView, didScrollToPercentageOffset: percentageHorizontalOffset)
+        
+        
+        /*
+         * below code scales the colorView on paging the scrollview
+         */
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        
+        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
+            
+            slides[0].colorView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
+            slides[1].colorView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
+            slides[1].colorView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
+            slides[2].colorView.transform = CGAffineTransform(scaleX: percentOffset.x/0.50, y: percentOffset.x/0.50)
+            
+        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
+            slides[2].colorView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.25, y: (0.75-percentOffset.x)/0.25)
+            slides[3].colorView.transform = CGAffineTransform(scaleX: percentOffset.x/0.75, y: percentOffset.x/0.75)
+            
+        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
+            slides[3].colorView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
+            slides[4].colorView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
+        }
+    }
+}
